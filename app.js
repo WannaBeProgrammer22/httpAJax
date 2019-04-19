8278d447ad523af8c2065298500780050c649405
Jerwin Fernandez <jerwinfernandez04.gmail.com>
2019-04-19 14:41:14 +0800

    done ajax http library

--- app.js
+++ app.js
@@ -1,9 +1,52 @@
 const http = new EasyHTTP();
 
-const posts = http.get('https://jsonplaceholder.typicode.com/posts', function (err, post) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(post);
-    }
-});
+// get posts
+// http.get('https://jsonplaceholder.typicode.com/posts', function (err, post) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(post);
+//     }
+// });
+
+// get single post
+// http.get('https://jsonplaceholder.typicode.com/posts/1', function (err, post) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(post);
+//     }
+// });
+
+// create data
+const data = {
+    title: 'Custom Post',
+    body: 'This is a custom post'
+};
+
+// // create post
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function (err, post) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(post);
+//     }
+// });
+
+// Update Post
+// http.put('https://jsonplaceholder.typicode.com/posts/1', data, function (err, post) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(post);
+//     }
+// });
+
+// Delete post
+// http.delete('https://jsonplaceholder.typicode.com/posts/1', function (err, response) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(response);
+//     }
+// });
--- easyhttp.js
+++ easyhttp.js
@@ -7,8 +7,7 @@ function EasyHTTP() {
 EasyHTTP.prototype.get = function (url, callback) {
     this.http.open('GET', url, true);
 
-    // using self capturing the instance of this
-    // from the current scope
+    // capturing this to get the current instance
     let self = this;
     this.http.onload = function () {
         if (self.http.status === 200) {
@@ -17,10 +16,60 @@ EasyHTTP.prototype.get = function (url, callback) {
             callback('Error: ' + self.http.status);
         }
     }
+
     this.http.send();
 }
+
 // make an HTTP POST request
+EasyHTTP.prototype.post = function (url, data, callback) {
+    this.http.open('POST', url, true);
+
+    // need to set content type - the data type working with
+    this.http.setRequestHeader('Content-type', 'application/json');
+
+    let self = this;
+    this.http.onload = function () {
+        // this will be the new post
+        callback(null, self.http.responseText);
+    }
+
+    // send data object and make it json string
+    this.http.send(JSON.stringify(data));
+}
 
 // make an HTTP PUT request
+EasyHTTP.prototype.put = function (url, data, callback) {
+    this.http.open('PUT', url, true);
+
+    // need to set content type - the data type working with
+    this.http.setRequestHeader('Content-type', 'application/json');
+
+    let self = this;
+    this.http.onload = function () {
+        // this will be the new post
+        callback(null, self.http.responseText);
+    }
+
+    // send data object and make it json string
+    this.http.send(JSON.stringify(data));
+}
 
 // make an HTTP DELETE request
+EasyHTTP.prototype.delete = function (url, callback) {
+    this.http.open('DELETE', url, true);
+
+    let self = this;
+    this.http.onload = function () {
+        if (self.http.status === 200) {
+            // return empty {} 
+            // callback(null, self.http.responseText);
+
+            // return post deleted message
+            callback(null, 'Post deleted');
+        } else {
+            callback('Error: ' + self.http.status);
+        }
+    }
+
+    this.http.send();
+}
